6d3370d9a02773379fee367044ae1e440df11f7f
SvalovaNastya <Sun-7402@mail.ru>
2016-03-08 20:52:09 +0500

    еще немножко рефакторинга

--- Probation/Svalova_Nastya.cs
+++ Probation/Svalova_Nastya.cs
@@ -26,7 +26,8 @@ namespace Probation
             {'Y', CardColors.Yellow},
             {'W', CardColors.White}
         };
-        
+
+        // Возможно не в этой задаче, но в игре в целом конструктор все же понадобится
         public Card(int rank, CardColors color)
         {
             Rank = rank;
@@ -42,46 +43,51 @@ namespace Probation
     public class CardInfo
     {
         public readonly Card RealCard;
-        public bool KnewRank { get; private set; }
-        public bool KnewColor { get; private set; }
-        public HashSet<int> NotThisRank { get; private set; } 
-        public HashSet<CardColors> NotThisColor { get; private set; }
+        public bool RankIsKnown { get; private set; }
+        public bool ColorIsKnown { get; private set; }
+        public HashSet<int> NotThisRanks { get; set; } 
+        public HashSet<CardColors> NotThisColors { get; private set; }
 
         public CardInfo(Card realCard)
         {
             RealCard = realCard;
-            KnewRank = false;
-            KnewColor = false;
-            NotThisRank = new HashSet<int>();
-            NotThisColor = new HashSet<CardColors>();
+            RankIsKnown = false;
+            ColorIsKnown = false;
+            NotThisRanks = new HashSet<int>();
+            NotThisColors = new HashSet<CardColors>();
         }
 
-        public bool TryAddInfoAboutCard(int? rank, CardColors? color, int? notRank, CardColors? notColor)
+        public bool TryAddInfoAboutRank(int? rank, int? notRank)
         {
             if (rank.HasValue)
                 if (rank.Value != RealCard.Rank)
                     return false;
                 else
-                    KnewRank = true;
-            if (color.HasValue)
-                if (color.Value != RealCard.Color)
-                    return false;
-                else
-                    KnewColor = true;
+                    RankIsKnown = true;
             if (notRank.HasValue)
                 if (RealCard.Rank == notRank.Value)
                     return false;
                 else
-                    NotThisRank.Add(notRank.Value);
+                    NotThisRanks.Add(notRank.Value);
+            if (NotThisRanks.Count == 4)
+                RankIsKnown = true;
+            return true;
+        }
+
+        public bool TryAddInfoAboutColor(CardColors? color, CardColors? notColor)
+        {
+            if (color.HasValue)
+                if (color.Value != RealCard.Color)
+                    return false;
+                else
+                    ColorIsKnown = true;
             if (notColor.HasValue)
                 if (RealCard.Color == notColor.Value)
                     return false;
                 else
-                    NotThisColor.Add(notColor.Value);
-            if (NotThisRank.Count == 4)
-                KnewRank = true;
-            if (NotThisColor.Count == 4)
-                KnewColor = true;
+                    NotThisColors.Add(notColor.Value);
+            if (NotThisColors.Count == 4)
+                ColorIsKnown = true;
             return true;
         }
     }
@@ -114,9 +120,9 @@ namespace Probation
             cards.Add(card);
         }
 
-        public void AddCards(CardInfo[] cards)
+        public void AddCards(IEnumerable<CardInfo> addendCards)
         {
-            foreach (var card in cards)
+            foreach (var card in addendCards)
             {
                 AddCard(card);
             }
@@ -135,16 +141,16 @@ namespace Probation
 
         public bool TryTellRank(int rank, List<int> selectedCardsIndexes)
         {
-            for (int i = 0; i < cards.Count; i++)
+            for (var i = 0; i < cards.Count; i++)
             {
                 if (selectedCardsIndexes.Contains(i))
                 {
-                    if (!cards[i].TryAddInfoAboutCard(rank, null, null, null))
+                    if (!cards[i].TryAddInfoAboutRank(rank, null))
                         return false;
                 }
                 else
                 {
-                    if (!cards[i].TryAddInfoAboutCard(null, null, rank, null))
+                    if (!cards[i].TryAddInfoAboutRank(null, rank))
                         return false;
                 }
             }
@@ -153,16 +159,16 @@ namespace Probation
 
         public bool TryTellColor(CardColors color, List<int> selectedCardsIndexes)
         {
-            for (int i = 0; i < cards.Count; i++)
+            for (var i = 0; i < cards.Count; i++)
             {
                 if (selectedCardsIndexes.Contains(i))
                 {
-                    if (!cards[i].TryAddInfoAboutCard(null, color, null, null))
+                    if (!cards[i].TryAddInfoAboutColor(color, null))
                         return false;
                 }
                 else
                 {
-                    if (!cards[i].TryAddInfoAboutCard(null, null, null, color))
+                    if (!cards[i].TryAddInfoAboutColor(null, color))
                         return false;
                 }
             }
@@ -172,7 +178,7 @@ namespace Probation
 
     public class GameProcessor
     {
-        private const int playersCount = 2;
+        private const int PlayersCount = 2;
 
         private Dictionary<CardColors, int> cardsOnTable;
         private Queue<Card> deck; 
@@ -191,31 +197,30 @@ namespace Probation
                 action();
                 if (gameIsOver && !gameWasOver)
                     yield return new GameResult(turnsCount, playedCards, riskedTurns);
-                currentPlayer = (currentPlayer + 1) % playersCount;
+                currentPlayer = (currentPlayer + 1) % PlayersCount;
                 turnsCount++;
             }
         }
 
-        public void StartNewGame(List<string> initalCards)
+        public void StartNewGame(List<Card> initalCards)
         {
             InitGameState();
-            for (int i = 0; i < playersCount; i++)
+            for (var i = 0; i < PlayersCount; i++)
             {
                 playersHands[i].AddCards(initalCards
                     .Skip(i*5)
                     .Take(5)
-                    .Select(x => new CardInfo(Card.SelectAbbreviationToCard(x)))
-                    .ToArray());
+                    .Select(card => new CardInfo(card)));
             }
-            deck = new Queue<Card>(initalCards.Skip(5*playersCount).Select(Card.SelectAbbreviationToCard));
+            deck = new Queue<Card>(initalCards.Skip(5*PlayersCount));
         }
 
         private void InitGameState()
         {
             gameIsOver = false;
             currentPlayer = -1;
-            playersHands = new PlayerHand[playersCount];
-            for (int i = 0; i < playersCount; i++)
+            playersHands = new PlayerHand[PlayersCount];
+            for (var i = 0; i < PlayersCount; i++)
             {
                 playersHands[i] = new PlayerHand();
             }
@@ -234,16 +239,13 @@ namespace Probation
 
         private int GetNextPlayer()
         {
-            return (currentPlayer + 1)%playersCount;
+            return (currentPlayer + 1)%PlayersCount;
         }
 
-        public void TellColor(string colorString, List<int> selectedCardsIndexes)
+        public void TellColor(CardColors color, List<int> selectedCardsIndexes)
         {
             if (gameIsOver)
                 return;
-            CardColors color;
-            if(!Enum.TryParse(colorString, true, out color))
-                throw new Exception("incorrect input format");
             var nextPlayersHand = playersHands[GetNextPlayer()];
             if (!nextPlayersHand.TryTellColor(color, selectedCardsIndexes))
                 gameIsOver = true;
@@ -272,7 +274,7 @@ namespace Probation
         {
             if (gameIsOver)
                 return;
-            CardInfo card = playersHands[currentPlayer].GetCard(cardsIndex);
+            var card = playersHands[currentPlayer].GetCard(cardsIndex);
             if (cardsOnTable[card.RealCard.Color] + 1 != card.RealCard.Rank)
             {
                 gameIsOver = true;
@@ -291,16 +293,18 @@ namespace Probation
 
         private bool CheckForRisked(CardInfo card)
         {
-            if (card.KnewRank && card.RealCard.Rank == 1)
+            if (card.RankIsKnown && card.RealCard.Rank == 1)
             {
-                if (card.KnewColor || playedCards == 0)
+                if (card.ColorIsKnown || playedCards == 0)
                     return false;
             }
-            if (card.KnewColor && card.KnewRank)
+            if (card.ColorIsKnown && card.RankIsKnown)
                 return false;
-            if (card.KnewRank)
+            if (card.RankIsKnown)
             {
-                return cardsOnTable.Keys.Where(cardColor => cardsOnTable[cardColor] != card.RealCard.Rank - 1).Any(cardColor => !card.NotThisColor.Contains(cardColor));
+                return cardsOnTable.Keys
+                    .Where(cardColor => cardsOnTable[cardColor] != card.RealCard.Rank - 1)
+                    .Any(cardColor => !card.NotThisColors.Contains(cardColor));
             }
             return true;
         }
@@ -320,42 +324,64 @@ namespace Probation
 
     public class GameCommandsParser
     {
-        private readonly Regex StartNewGameRegex = new Regex(@"Start new game with deck (.*)");
-        private readonly Regex TellColorRegex = new Regex(@"Tell color (\w*) for cards (.*)");
-        private readonly Regex TellRankRegex = new Regex(@"Tell rank (\d) for cards (.*)");
-        private readonly Regex PlayCardRegex = new Regex(@"Play card (\d)");
-        private readonly Regex DropCardRegex = new Regex(@"Drop card (\d)");
-        private readonly Dictionary<Regex, Action<Match>> InputDataProсessing;
+        private readonly Regex startNewGameRegex = new Regex(@"Start new game with deck (.*)");
+        private readonly Regex tellColorRegex = new Regex(@"Tell color (\w*) for cards (.*)");
+        private readonly Regex tellRankRegex = new Regex(@"Tell rank (\d) for cards (.*)");
+        private readonly Regex playCardRegex = new Regex(@"Play card (\d)");
+        private readonly Regex dropCardRegex = new Regex(@"Drop card (\d)");
+        private readonly Dictionary<Regex, Action<Match>> inputDataProсessing;
 
         public GameCommandsParser(GameProcessor processor)
         {
-            InputDataProсessing = new Dictionary<Regex, Action<Match>>();
-            InputDataProсessing[StartNewGameRegex] = match => processor.StartNewGame(match.Groups[1].Value.Split(' ').ToList());
-            InputDataProсessing[TellColorRegex] = match => processor.TellColor(match.Groups[1].Value, match.Groups[2].Value.Split(' ').Select(int.Parse).ToList());
-            InputDataProсessing[TellRankRegex] = match => processor.TellRank(int.Parse(match.Groups[1].Value), match.Groups[2].Value.Split(' ').Select(int.Parse).ToList());
-            InputDataProсessing[PlayCardRegex] = match => processor.PlayCard(int.Parse(match.Groups[1].Value));
-            InputDataProсessing[DropCardRegex] = match => processor.DropCard(int.Parse(match.Groups[1].Value));
+            inputDataProсessing = new Dictionary<Regex, Action<Match>>();
+            inputDataProсessing[startNewGameRegex] = match =>
+            {
+                processor.StartNewGame(
+                    match.Groups[1].Value.Split(' ')
+                        .Select(Card.SelectAbbreviationToCard)
+                        .ToList());
+            };
+            inputDataProсessing[tellColorRegex] = match =>
+            {
+                processor.TellColor(ParseColorString(match.Groups[1].Value),
+                    match.Groups[2].Value.Split(' ').Select(int.Parse).ToList());
+            };
+            inputDataProсessing[tellRankRegex] = match =>
+            {
+                processor.TellRank(int.Parse(match.Groups[1].Value),
+                    match.Groups[2].Value.Split(' ').Select(int.Parse).ToList());
+            };
+            inputDataProсessing[playCardRegex] = match => { processor.PlayCard(int.Parse(match.Groups[1].Value)); };
+            inputDataProсessing[dropCardRegex] = match => { processor.DropCard(int.Parse(match.Groups[1].Value)); };
         }
 
         public Action ParseCommand(string commandLine)
         {
-            foreach (var regex in InputDataProсessing.Keys)
+            foreach (var regex in inputDataProсessing.Keys)
             {
                 if (regex.IsMatch(commandLine))
                 {
                     var currentRegex = regex;
-                    return () => InputDataProсessing[currentRegex](currentRegex.Match(commandLine));
+                    return () => inputDataProсessing[currentRegex](currentRegex.Match(commandLine));
                 }
             }
             throw new Exception("Error input line");
         }
+
+        public CardColors ParseColorString(string colorString)
+        {
+            CardColors color;
+            if (!Enum.TryParse(colorString, true, out color))
+                throw new Exception("incorrect input format");
+            return color;
+        }
     }
 
-    public class Program
+    public static class Program
     {
-        public static IEnumerable<string> ReadAllLines()
+        private static IEnumerable<string> ReadAllLines()
         {
-            string line = Console.ReadLine();
+            var line = Console.ReadLine();
             while (line != null)
             {
                 yield return line;
